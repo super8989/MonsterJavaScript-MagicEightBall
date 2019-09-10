c7354ed02a4bb874814e0f93b9f3f554293e870e
Sam <super8989@gmail.com>
2019-09-09 23:08:58 -0400

    responding to questions

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const message = document.querySelector(".message");
 const question = document.querySelector("input");
 const button = document.querySelector("button");
+
+const answerArray = ["Maybe", "Probably", "Highly Likely", "I don't know", "Too bad", "Don't Count on it", "Next", "Good luck"];
+
+button.addEventListener('click', function() {
+    console.log(question.value);
+    let res = Math.floor(Math.random() * answerArray.length);
+    console.log(answerArray[res]);
+
+    message.innerText = question.value + " " + answerArray[res];
+    question.value = "";
+
+
+})
